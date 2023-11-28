21fddb2827654ca8eb6a74a0f251ae66600b8d7e
Daniel Brandt <dbbrandt@gmail.com>
2023-11-28 15:11:06 -0500

    Adding basic redux code for an action and reducers. Added Provider to index.tsx.

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function

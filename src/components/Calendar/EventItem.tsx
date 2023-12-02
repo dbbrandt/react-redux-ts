c23c4fdb4089635f95cdea84e2620d86fb89320c
Daniel Brandt <dbbrandt@gmail.com>
2023-12-01 19:27:51 -0500

    Added ability to click anywhere on the event div to enter edit mode. To get focus on the input field to work  had  to put it in a useEffect because the onClick event caused the component to rerender a remove  the focus. Fixed time range to be generated from actual start and end.

--- src/components/Calendar/EventItem.tsx
+++ src/components/Calendar/EventItem.tsx
@@ -1,6 +1,13 @@
 import {deleteEvent, updateEvent, UserEvent} from "./user-events-slice";
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useEffect, useRef, useState} from "react";
 import {useAppDispatch} from "../../redux/store";
+import {addZero} from "../../lib/utils";
+
+
+const createTime = (dateString: string) => {
+    const date = new Date(dateString);
+    return  `${addZero(date.getHours())}:${addZero(date.getMinutes())}`;
+};
 
 interface Props {
     eventItem: UserEvent
@@ -9,14 +16,23 @@ interface Props {
 const EventItem : React.FC<Props> = ({ eventItem}) => {
     const dispatch = useAppDispatch();
     const [title, setTitle] = useState<UserEvent['title']>(eventItem.title);
+    const [editing, setEditing] = useState<boolean>(false);
+    const inputRef = useRef<HTMLInputElement>(null)
     const handleDelete = () => {
         dispatch(deleteEvent(eventItem.id));
     }
+
+    useEffect(() => {
+        if (editing && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [editing]);
 
     const handleUpdate = () => {
         const updated = {...eventItem};
         updated.title = title;
         dispatch(updateEvent(updated));
+        setEditing(false);
     }
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -24,18 +40,27 @@ const EventItem : React.FC<Props> = ({ eventItem}) => {
     };
 
     return (
-        <div className="calendar-event">
+        <div className="calendar-event"
+             tabIndex={0}  // Make the div focusable
+             onClick={() => setEditing(true)}
+             style={{ outline: 'none' }}  // Optionally remove the outline on focus
+        >
             <div className="calendar-event-info">
-                <div className="calendar-event-time">10:00 - 12:00</div>
-                <div className="calendar-event-title">
+                <div className="calendar-event-time">
+                    {createTime(eventItem.dateStart)} - {createTime(eventItem.dateEnd)}
+                </div>
+                <div className="calendar-event-title"  style={{display: editing ? 'block' : 'none'}}>
                     <input
                         type="text"
                         value={title}
-
-                        onChange={handleChange}
+                        ref={inputRef}
                         onBlur={handleUpdate}
+                        onChange={handleChange}
                     />
                 </div>
+            </div>
+            <div className="calendar-event-title" style={{display: editing ? 'none' : 'block'}}>
+                {eventItem.title}
             </div>
             <button className="calendar-event-delete-button" onClick={handleDelete}>
                 &times;
